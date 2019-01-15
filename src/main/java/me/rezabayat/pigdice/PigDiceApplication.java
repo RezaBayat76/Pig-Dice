b1824bc29a228779a4e535bf67e761cd5fb3709f
reza <reza.bayat76@gmail.com>
2019-01-15 23:09:00 +0330

    initial project

---
+++ src/main/java/me/rezabayat/pigdice/PigDiceApplication.java
@@ -0,0 +1,14 @@
+package me.rezabayat.pigdice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigDiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigDiceApplication.class, args);
+	}
+
+}
+
